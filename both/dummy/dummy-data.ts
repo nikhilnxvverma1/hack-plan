a5b44743d0a55dacc7239ba928d6dae07ce523c3
Nikhil Verma <nikhilnxvverma1@aol.com>
2016-10-08 20:56:22 -0400

    dummy data and design work (in progress)

---
+++ both/dummy/dummy-data.ts
@@ -0,0 +1,30 @@
+export const dummyEvent={
+    name:'Big Red Hacks',
+    venue:'Cornell University',
+};
+
+export const dummyProject={
+    name:"Hack Plan",
+    participants:[
+        {
+            name:"Nikhil Verma",
+            tasks:[
+                {
+                    name:"homepage",
+                    taskList:[
+                        {
+                            description:"Setup configurations for cdn"
+                        }
+                    ]
+                }
+
+            ]
+        },
+        {
+            name:"Ujwal Bharat"
+        },
+        {
+            name:"Alvin"
+        }
+    ]
+};
--- client/imports/app/planner.component.ts
+++ client/imports/app/planner.component.ts
@@ -1,6 +1,6 @@
 import { Component, Directive, Output, EventEmitter } from '@angular/core';
 import template from './template/planner.component.html';
-
+import { dummyEvent } from '../../../both/dummy/dummy-data';
 
 @Component({
     selector: 'planner',
@@ -8,6 +8,14 @@ import template from './template/planner.component.html';
 })
 export class PlannerComponent {
 
+    eventPlan=dummyEvent;
+
     constructor(){
     }
 }
+
+//class Event{
+//    name:string;
+//    venue:string;
+//
+//}
