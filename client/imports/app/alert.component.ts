8e899098f38654615827f81cdaa46f082459d0a6
Ujwal Bharat <ujwalbharatn@gmail.com>
2016-10-09 01:56:43 -0400

    Added alert component (Still testing)

---
+++ client/imports/app/alert.component.ts
@@ -0,0 +1,18 @@
+import {Component,OnInit} from '@angular/core';
+import template from './template/alert.component.html';
+
+@Component({
+    selector: 'alert',
+    inputs: ['message'],
+    template
+})
+
+export class AlertComponent implements OnInit{
+    public message = {
+    'header': 'Hello',
+    'text': 'This is some text'
+    }
+    ngOnInit(){
+        $('.modal').leanModal();
+    }
+}
